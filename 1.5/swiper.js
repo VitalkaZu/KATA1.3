b56d0deb57c1d9dd820abea21322cae0cd0fc9c2
VitalkaZu <vitalkazu@gmail.com>
2022-08-17 16:48:56 +0800

    add swiper

---
+++ 1.5/swiper.js
@@ -0,0 +1,12 @@
+new Swiper(".swiper-container",{
+    // If we need pagination
+    pagination: {
+        el: '.swiper-pagination',
+    },
+
+    // Navigation arrows
+    navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
+    },
+});
