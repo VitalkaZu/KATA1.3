815653bc6bc7305d02d8e3c6e0213c00ff41e818
VitalkaZu <vitalkazu@gmail.com>
2022-08-19 00:27:48 +0800

    fix 1.5

--- 1.5/collapse.js
+++ 1.5/collapse.js
@@ -1,16 +1,13 @@
-let button = document.querySelector(".button");
-let buttonText = button.querySelector(".button__text");
-let collapseArea = document.querySelector(".swiper-list");
+const button = document.querySelector(".button");
+const buttonText = button.querySelector(".button__text");
+const collapseArea = document.querySelector(".swiper-list");
 
 button.addEventListener("click",function () {
     if (buttonText.textContent == "Показать все") {
-        // collapseArea.classList.add("brands-list--open")
         collapseArea.style.maxHeight = collapseArea.scrollHeight +  "px";
-        // collapseArea.setAttribute("style","max-height:400px");
         buttonText.textContent = "Скрыть";
         button.classList.add("button--reverse");
     } else {
-        // collapseArea.classList.remove("brands-list--open")
         collapseArea.removeAttribute("style");
         button.classList.remove("button--reverse");
         buttonText.textContent = "Показать все";
