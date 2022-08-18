ddb0de4799597a7931d40d5f70c7c0e4abc54922
VitalkaZu <vitalkazu@gmail.com>
2022-08-18 16:30:11 +0800

    fix slider

--- 1.5/collapse.js
+++ 1.5/collapse.js
@@ -1,13 +1,16 @@
 let button = document.querySelector(".button");
 let buttonText = button.querySelector(".button__text");
-let collapseArea = document.querySelector(".brands");
+let collapseArea = document.querySelector(".swiper-list");
 
 button.addEventListener("click",function () {
     if (buttonText.textContent == "Показать все") {
-        collapseArea.setAttribute("style","max-height:400px");
+        // collapseArea.classList.add("brands-list--open")
+        collapseArea.style.maxHeight = collapseArea.scrollHeight +  "px";
+        // collapseArea.setAttribute("style","max-height:400px");
         buttonText.textContent = "Скрыть";
         button.classList.add("button--reverse");
     } else {
+        // collapseArea.classList.remove("brands-list--open")
         collapseArea.removeAttribute("style");
         button.classList.remove("button--reverse");
         buttonText.textContent = "Показать все";
--- 1.5/swiper.js
+++ 1.5/swiper.js
@@ -1,12 +1,34 @@
-new Swiper(".swiper-container",{
-    // If we need pagination
-    pagination: {
-        el: '.swiper-pagination',
-    },
+var swiper = Swiper;
+var init = false;
+function swiperMode() {
+    let mobile = window.matchMedia("(min-width: 0px) and (max-width: 768px)");
+    let swiperList = document.querySelector("#brands-list");
+    if (mobile.matches) {
+        if (!init) {
+            init = true;
+            swiperList.classList.remove("swiper-list");
+            swiper = new Swiper(".swiper-container", {
 
-    // Navigation arrows
-    navigation: {
-        nextEl: '.swiper-button-next',
-        prevEl: '.swiper-button-prev',
-    },
+                slidesPerView: "auto",
+
+                spaceBetween: 16,
+                pagination: {
+                    el: ".swiper-pagination",
+                    clickable: true,
+                },
+            });
+        }
+    } else {
+        swiperList.classList.add("swiper-list");
+        swiper.destroy();
+        init = false;
+    }
+}
+
+window.addEventListener("load", function () {
+    swiperMode();
+});
+
+window.addEventListener("resize", function () {
+    swiperMode();
 });
